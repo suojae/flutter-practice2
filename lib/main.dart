f5ce10e81a934df589dda2965fd3b7d433a50896
suojae <suojae.developer@gmail.com>
2024-12-06 22:20:14 +0900

    feat: signin-view 메인 반영

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
+import 'package:fire_walker/sign_in_page/sign_in_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// import 'main_page/main_view.dart';
+
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fire Runner',
+      debugShowCheckedModeBanner: false,
+      title: 'Fire Walker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF0E091C),
+        textTheme: GoogleFonts.passionOneTextTheme(),
       ),
-      // home: HomeScreen(),
+      home: const SignInView(),
+      // home: MainView(),
     );
   }
 }
